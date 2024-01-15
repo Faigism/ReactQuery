c491fb3b96340c86753fad7b3cc4d53c00278f62
Faigism <faigyi@code.edu.az>
2024-01-15 18:56:43 +0400

    render data and error message

--- src/Items.jsx
+++ src/Items.jsx
@@ -1,11 +1,28 @@
-import SingleItem from './SingleItem';
-const Items = ({ items }) => {
+import { isError, useQuery } from '@tanstack/react-query'
+import SingleItem from './SingleItem'
+import customFetch from './utils'
+
+const Items = () => {
+  const { isLoading, data, isError, error } = useQuery({
+    queryKey: ['tasks'],
+    queryFn: async () => {
+      const { data } = await customFetch.get('/something')
+      return data
+    },
+  })
+  if (isLoading) {
+    return <p style={{ marginTop: '1rem' }}>Loading...</p>
+  }
+  console.log(error)
+  if (error) {
+    return <p style={{ marginTop: '1rem' }}>{error.response.data}</p>
+  }
   return (
-    <div className='items'>
-      {items.map((item) => {
-        return <SingleItem key={item.id} item={item} />;
+    <div className="items">
+      {data.taskList.map((item) => {
+        return <SingleItem key={item.id} item={item} />
       })}
     </div>
-  );
-};
-export default Items;
+  )
+}
+export default Items
